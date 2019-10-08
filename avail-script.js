e893fb2cd3042ed69b00b544d7af66d9d015f722
Chris Scott <christo9090@gmail.com>
2019-10-08 15:36:13 -0600

    Styled, fully working. Dates sort in order.

--- avail-script.js
+++ avail-script.js
@@ -35,23 +35,24 @@ const getAvailableSlots = async () => {
       const jsonResponse = await response.json();
       console.log(jsonResponse);
       let slots2 = jsonResponse.slots
-      // slots2.push(jsonResponse.slots[0])
-      console.log(slots2)
-
       //loop through inspectors
       for (i = 0; i < slots2.length; i++) {
-
         if (inspectors.indexOf(slots2[i].userid) !== -1) {
           slots.push(slots2[i]);
-          // console.log('yes')
         }
       };
-      console.log(slots)
+      //sort the array by date.
+      slots.sort(function(a, b) {
+       var dateA = new Date(a.start), dateB = new Date(b.start);
+       return dateA - dateB;
+      });
+      //If no availability return the following.
       if (slots.length > 0) {
         returnTimes()
       } else {
-        document.getElementById('availableSlotsReturn').innerHTML = '<p>Sorry, there are no inspection slots available in the next 5 days for a pre-purchase home inspection in that zip code.</p><br><p>Give us a call for more options.';
+        document.getElementById('availableSlotsReturn').innerHTML = '<p>Sorry, there are no inspection slots available in the next 5 days for a pre-purchase home inspection in that zip code.</p><p>Give us a call for more options.';
       };
+
 
     } else {
   throw new Error('Request Failed!')}
@@ -60,12 +61,16 @@ const getAvailableSlots = async () => {
   }
 };
 
+// Function that modifies the DOM to return the call.
 const returnTimes = () => {
   let finalReturn = []
   for (i = 0; i < slots.length; i++) {
     finalReturn.push(slots[i].start + " with " + slots[i].userdisplay + "<br>")
   };
-  document.getElementById('availableSlotsReturn').innerHTML = `<p>Available Slots in the next 5 days:<br> ${finalReturn.join('')}</p>`;
+  document.getElementById('availableSlotsReturn').innerHTML = `<p><strong>Available Slots in the next 5 days:</strong><br> ${finalReturn.join('')}</p>`;
+  slots = [];
+  finalReturn = [];
+  newFinalReturn = [];
 };
 
 document.getElementById("submitbutton").onclick = function() {getAvailableSlots()};
